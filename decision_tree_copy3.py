33d228801979daec1c26142ceb6fd9595532c86f
frankligy <59619831+frankligy@users.noreply.github.com>
2020-02-04 11:47:23 -0500

    Add files via upload

--- decision_tree_copy3.py
+++ decision_tree_copy3.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Tue Feb  4 11:27:20 2020
+
+@author: ligk2e
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Sat Feb  1 21:09:20 2020
 
 @author: ligk2e
@@ -25,10 +33,10 @@ def split_train_test(train_num,seed,df):
     random.seed(seed)
     train = np.array(random.sample(range(310),train_num))
     test = np.delete(range(310),train)
-    X_train = df.values[train,:6]
-    Y_train = df.values[train,6]
-    X_test = df.values[test,:6]
-    Y_test = df.values[test,6]
+    X_train = df.values[train,:5]
+    Y_train = df.values[train,5]
+    X_test = df.values[test,:5]
+    Y_test = df.values[test,5]
     return X_train,X_test,Y_train,Y_test
 
 # X2_train,X2_test,Y2_train,Y2_test = train_test_split(X,y,test_size=0.3,random_state=1)
@@ -45,7 +53,7 @@ def decision_tree2(class1,class2):
     #    clf.predict_proba([[2,2,2,2,2,2]])
         dot_data = StringIO(tree.export_graphviz(clf,out_file=None))
         graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-        graph.write_png('/Users/ligk2e/Desktop/IDA1/tree{}.png'.format(leafnode_length[i]))
+        graph.write_png('/Users/ligk2e/Desktop/IDA1/q3/tree{}.png'.format(leafnode_length[i]))
         
         Y2_pred = clf.predict(X2_test)
         accuracy.append(metrics.accuracy_score(Y2_test,Y2_pred))
@@ -78,29 +86,31 @@ def draw_dt2(class1,class2,accuracy,precision,recall,path):
 
 def truncating_df(df):
     df['dicho_class'] = ''
-    df.loc[data2['class']=='Abnormal','dicho_class'] = 1
-    df.loc[data2['class']=='Normal','dicho_class'] = 0
+    df.loc[df['class']=='Abnormal','dicho_class'] = 1
+    df.loc[df['class']=='Normal','dicho_class'] = 0
     df_new = df.drop('class',axis=1)
     correlation = df_new.corr()
     corr_with_class = list(correlation.loc['dicho_class'])
     abs_value = [abs(ele) for ele in corr_with_class]
-    temp = abs_value.sort(reverse=True)[1]
+    #print(abs_value)
+    temp = sorted(abs_value,reverse=True)[1]
     temp_index = corr_with_class.index(temp)
     name=list(df_new.columns)[temp_index]
-    df_new2 = df_new.drop(name,axis=1)
+    df_new2 = df.drop([name,'dicho_class'],axis=1)
     return df_new2
     
     
     
 if __name__ == '__main__':
-    data2 = pd.read_csv('/Users/ligk2e/Desktop/IDA1/Biomechanical_Data_2Classes.csv',sep=',')
+    df = pd.read_csv('/Users/ligk2e/Desktop/IDA1/Biomechanical_Data_2Classes.csv',sep=',')
     leafnode_length = [5,15,25,40,50]
-    X2_train,X2_test,Y2_train,Y2_test = split_train_test(230,3,data2)
+    data4 = truncating_df(df)
+    X2_train,X2_test,Y2_train,Y2_test = split_train_test(230,3,data4)
     accuracy,precision,recall = decision_tree2('Abnormal','Normal')
-    draw_dt2('Abnormal','Normal',accuracy,precision,recall,'/Users/ligk2e/Desktop/IDA1/metrics.svg')
+    draw_dt2('Abnormal','Normal',accuracy,precision,recall,'/Users/ligk2e/Desktop/IDA1/q3/metrics.svg')
     
     
-    
+
     
 
 
