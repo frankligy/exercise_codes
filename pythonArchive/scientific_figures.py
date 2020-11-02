243bee0669c26f280a1ecb0aa837b9d6eb048f35
Guangyuan Li <Frank Li>
2020-11-02 17:06:51 -0500

    draw

--- pythonArchive/scientific_figures.py
+++ pythonArchive/scientific_figures.py
@@ -5,7 +5,6 @@ This script is to play how to use python to generate scientific figures
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-from pylab import cm
 
 mpl.rcParams['pdf.fonttype'] = 42
 mpl.rcParams['ps.fonttype'] = 42
@@ -28,7 +27,10 @@ if __name__ == '__main__':
     font_names = [f.name for f in fm.fontManager.ttflist]
     print(font_names)  # check all available font in my computer
 
-    colors = cm.get_cmap('tab10',2)
+    # for color
+    plt.colormaps()  # all color maps, just specify the string
+    from matplotlib.colors import ListedColormap
+    palette = ListedColormap(['g','r'])  # a ListedColorMap object
 
 
     # remove spines
@@ -36,6 +38,9 @@ if __name__ == '__main__':
     axes1.spines['top'].set_visible(False)
 
     # ticks
+    axes1.xaxis.set_major_locator()  # a locator object, mpl.ticker
+    axes1.set_xticks()
+    axes1.set_xticklabels()
     axes1.xaxis.set_tick_params(which='major',size=10,width=2,direction='in',top='on')
     '''
     which: major, minor, both
@@ -45,11 +50,8 @@ if __name__ == '__main__':
     top/right: 'on', will ticks show on the secondary axes (top and right spines)  
     '''
 
-    ax.plot(data,color=colors[0])
 
     ax.set_xlim(10,100)
-
-    ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(100))  # set the major ticks every multiple of 100, FixedLocator for mannually add ticks
 
 
     ax.set_xlabel(r'$\mathregular{\lambda}$',labelpad=10)   # labelpad: extra padding between the tick labels and the axis label
