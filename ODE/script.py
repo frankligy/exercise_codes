b82a81fc044ec5e18c1667c53f50bd16dfc57b73
Guangyuan Li <Frank Li>
2021-04-04 12:37:05 -0400

    ODE

--- ODE/script.py
+++ ODE/script.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy.integrate import odeint
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
+from gekko import GEKKO
 
 '''
 Figure1A linear signal response
@@ -302,6 +304,14 @@ S_options = np.linspace(0,15,100)
 def equation(R,S):
     EP = Goldbeter_Koshland(k3*R,k4,J3,J4)
     return k0*EP + k1*S - k2*R
+
+# it's necessary to first check the vector field
+fig,ax = plt.subplots()
+S = 10
+test = [equation(R,S) for R in np.linspace(0,1,100)]
+ax.plot(np.linspace(0,1,100),test)
+# you can find two stable steady state and an unstable steady state
+
 fig,ax = plt.subplots()
 store_uplimb,store_downlimb = [],[]
 for S in S_options:
@@ -309,18 +319,172 @@ for S in S_options:
     R_ss_downlimb = fsolve(func=equation,x0=[0],args=(S,))[0]
     store_uplimb.append(R_ss_uplimb)
     store_downlimb.append(R_ss_downlimb)
-ax.plot(S_options,store_uplimb)
-ax.plot(S_options,store_downlimb)
+ax.plot(S_options,store_uplimb,c='k')
+ax.plot(S_options[0:67],store_downlimb[0:67],c='k')
+ax.plot(np.linspace(0,10,5),[0.2,0.18,0.17,0.16,0.14],c='k',linestyle='--')
+ax.set_xlim(0,15)
+ax.set_ylim(0,0.7)
+ax.set_xlabel('Signal(S)')
+ax.set_ylabel('Response(R)')
+ax.text(x=7.5,y=0.65,s='Mutual activation',horizontalalignment='center',verticalalignment='center')
+plt.savefig('/Users/ligk2e/Desktop/ODE/Figure1e_SR_curve.pdf',bbox_inches='tight')
 
 
+'''
+Figure1f mutual inhibition
+'''
+k0=0
+k1=0.05
+k2=0.1
+k2_prime=0.5
+k3=1
+k4=0.2
+J3=0.05
+J4=0.05
 
-R = np.linspace(-1,2,10000)
-result = equation(R,S=5)
+# rate curve
+def Goldbeter_Koshland(v1,v2,J1,J2):
+    '''
+    will return the equalibrium concertration of either phosphorated or unphosphorated form of E,
+    v1,J1: incoming
+    v2,J2: outcoming
+    '''
+    B = v2-v1+J1*v2+J2*v1
+    equilibrium = 2*v1*J2/(B+np.sqrt(B**2-4*(v2-v1)*v1*J2))
+    return equilibrium
+R = np.linspace(0,1.5,100)
+S_options = [0.6,1.2,1.8]
+ER = Goldbeter_Koshland(k4, k3 * R, J3, J4)
+removal_rate = k2 * R + k2_prime * ER * R
 fig,ax = plt.subplots()
-ax.plot(R,result)
-ax.axhline(0,linestyle='--',c='k')
+ax.plot(R,removal_rate,color='k',linestyle='-')
+for S in S_options:
+    production_rate = [k0+k1*S] * 100
+    ax.plot(R,production_rate,color='k',linestyle='--')
+ax.set_xlim(0,1.5)
+ax.set_ylim(0,0.1)
+ax.set_xlabel('R')
+ax.set_ylabel('Rate(dR/dt)')
+ax.text(x=1.3,y=0.035,s='0.6',horizontalalignment='center',verticalalignment='center')
+ax.text(x=1.3,y=0.065,s='1.2',horizontalalignment='center',verticalalignment='center')
+ax.text(x=1.3,y=0.095,s='1.8',horizontalalignment='center',verticalalignment='center')
+plt.savefig('/Users/ligk2e/Desktop/ODE/figure1f_rate_curve.pdf',bbox_inches='tight')
+plt.close()
 
-fsolve(func=equation,x0=[0.0],args=(5,))
+# SR curve
+S_options = np.linspace(0,2,100)
+def equation(R,S):
+    ER = Goldbeter_Koshland(k4, k3 * R, J3, J4)
+    return k0+k1*S-k2*R-k2_prime*ER*R
+# peek this equation, traverse starts from 0 or from 1 will end up with two stable solution, one unstable in between
+S = 1
+R_options = np.linspace(0,1,100)
+result = [equation(R,S) for R in R_options]
+fig,ax=plt.subplots()
+ax.plot(R,result)
+# start to plot
+fig,ax=plt.subplots()
+uplimb,downlimb = [],[]
+for S in S_options:
+    uplimb.append(fsolve(func=equation,x0=[1],args=(S,))[0])
+    downlimb.append(fsolve(func=equation,x0=[0],args=(S,))[0])
+ax.plot(S_options[round(100*0.84/2):],uplimb[round(100*0.84/2):],c='k')
+ax.plot(S_options[:round(100*1.7/2)],downlimb[:round(100*1.7/2)],c='k')
+ax.plot(np.linspace(0.842,1.7,5),[0.285,0.25,0.23,0.20,0.17],c='k',linestyle='--')
+ax.set_xlim(0,2)
+ax.set_ylim(0,1)
+ax.set_xlabel('Signal(S)')
+ax.set_ylabel('Response(R)')
+ax.set_title('Mutual inhibition')
+plt.savefig('/Users/ligk2e/Desktop/ODE/figure1f_SR_curve.pdf',bbox_inches='tight')
+plt.close()
+
+
+'''
+figure1g homestatis, negative feedback
+'''
+k0=1
+k2=1
+k3=0.5
+k4=1
+J3=0.01
+J4=0.01
+
+# rate curve
+R_options = np.linspace(0,1,100)
+S_options = [0.5,1,1.5]
+ER = Goldbeter_Koshland(k3,k4*R,J3,J4)  # now k3 is the rate (E -> ER), damn
+production_rate = k0*ER
+fig,ax=plt.subplots()
+ax.plot(R_options,production_rate,c='k',linestyle='--')
+for S in S_options:
+    removal_rate = k2*S*R
+    ax.plot(R_options,removal_rate,c='k',linestyle='-')
+ax.set_xlim(0,1)
+ax.set_ylim(0,1.2)
+ax.set_xlabel('R')
+ax.set_ylabel('Rate(dR/dt)')
+plt.savefig('/Users/ligk2e/Desktop/ODE/figure1g_rate_curve.pdf',bbox_inches='tight')
+plt.close()
+
+# SR curve
+S_options=np.linspace(0.01,2,100)
+def equation(R,S):
+    ER=Goldbeter_Koshland(k3,k4*R,J3,J4)
+    return k0*ER - k2*S*R
+# for some reason, the fsolve can not approximate the root of above equation if you set x=1, (asymptotic issue)
+points = []
+for S in S_options:
+    points.append(fsolve(func=equation,x0=[1],args=(S,))[0])
+fig,ax = plt.subplots()
+ax.plot(S_options,points,c='k')
+ax.set_xlim(0,2)
+ax.set_ylim(0,1)
+ax.set_xlabel('Signal(S)')
+ax.set_ylabel('Response(R)')
+plt.savefig('/Users/ligk2e/Desktop/ODE/figure1g_SR_curve.pdf',bbox_inches='tight')
+plt.close()
+
+'''
+figure2a
+'''
+m = GEKKO()
+m.time = np.linspace(0,50,100)
+#S = m.Param(np.linspace(2,5,100))
+S = 4
+
+k0 = 0
+k1 = 1
+k2 = 0.01
+k2_prime = 10
+k3 = 0.1
+k4 = 0.2
+k5 = 0.1
+k6 = 0.05
+Yt=1
+Rt=1
+km3=0.01
+km4=0.01
+km5=0.01
+km6=0.01
+
+X = m.Var(value=0)
+Yp = m.Var(value=0)
+Rp = m.Var(value=0)
+
+m.Equation(X.dt() == k0+k1*S-k2*X+k2_prime*Rp*X)
+m.Equation(Yp.dt() == (k3*X*(Yt-Yp))/(km3+Yt-Yp) - (k4*Yp)/(km4+Yp))
+m.Equation(Rp.dt() == (k5*Yp*(Rt-Rp))/(km5+Rt-Rp) - (k6*Rp)/(km6+Rp))
+
+m.options.IMODE=4
+m.solve(disp=True)
+
+
+
+
+
+
+
 
 
 
