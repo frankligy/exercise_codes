a2681d6bd075f53f2f825ced8809d27abc17c31c
Guangyuan(Frank) Li <59619831+frankligy@users.noreply.github.com>
2022-06-23 16:01:28 -0400

    Update R_notes.R

--- R/R_notes.R
+++ R/R_notes.R
@@ -11,11 +11,18 @@ install.packages("BiocManager")
 ### download
 BiocManager::install("SummarizedExperiment")
 devtools::install_github('YingMa0107/CARD')
+
 ### debug
 # 1. in where R is installed, there's a Makeconf file can be editted (/Library/Frameworks/R.framework/Resources/etc/Makeconf)
 #    to configure the path to the gcc, clang, gfortran compiler.
 # 2. we can potentially create a file called ~/.R/Makevars to overwrite the setting in Makeconf, no guarantee will work
 # 3. install gfortran or other compilers (https://mac.r-project.org/tools/), add it to the path
+
+
+# we recommend using renv for environment management
+### In that way, R packages will be in: /Users/E0532183/cell_typing/renv/library/R-4.2/x86_64-apple-darwin17.0
+### If you want to run it outside of Rstudio: follow the instructions here: https://stackoverflow.com/questions/66311017/rscript-to-use-renv-environment
+
 
 # Basic R data type
 ### Please refer to https://towardsdatascience.com/learning-r-data-types-e698d23f8179
