c1311648981a686dfeb8a275d51538ffd2540400
Guangyuan(Frank) Li <59619831+frankligy@users.noreply.github.com>
2023-10-02 10:10:58 -0400

    add java

---
+++ java/learn_java/Main.java
@@ -0,0 +1,10 @@
+
+// you need to install jdk for compiling like javac or jre for just running it
+// jar is a archived file for running a program
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
